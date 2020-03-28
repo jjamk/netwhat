954b86e9f8848a7668d664a7e89a8bab4dbe66ca
Seohyeon Kang <seohyeonkang@Seohyeonui-MacBookPro.local>
2020-03-29 02:45:59 +0900

    t

--- test.c
+++ test.c
@@ -77,34 +77,3 @@ LData remove(List *plist)
 	(plist -> numOfData)--;
 	return (rdata);
 }
-#ifndef __D_LINKED_LIST_H__
-# define __D_LINKED_LIST_H__
-
-#define TRUE 1
-#define FALSE 0
-
-typedef int LData;
-
-typedef struct _node
-{
-	LData data;
-	struct _node *next;
-}		Node;
-
-typedef struct _linkedList
-{
-	Node *head;
-	Node *cur;
-	Node *before;	//노드의 삭제를 위해 존
-	int numOfData;
-	int (*comp)(Ldata d1, LData d2);
-}		LinkedList;
-
-typedef LinkedList List;
-
-void init(List *plist);
-void insert(List *plist, LData *pdata);
-
-int first(List *plist, LData *pdata);
-int next(List *plist, LData *pdata);
-
